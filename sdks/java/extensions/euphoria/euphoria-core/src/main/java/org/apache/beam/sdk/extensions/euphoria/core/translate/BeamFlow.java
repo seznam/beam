5b564e4811fdcf02882253587a67bfb51b72c6a4
vaclav.plajt@gmail.com <vaclav.plajt@firma.seznam.cz>
2018-08-07 13:54:44 +0200

    [BEAM-5099] `Pair` and `PairCoder` removed in favor of `KV`. Unnecesarry mapping from `Pair` to `KV` in `ReduceByKeyTranslator`removed. Some other references to `Pair` in comments and methods or variable names modified to `KV`.

--- sdks/java/extensions/euphoria/euphoria-core/src/main/java/org/apache/beam/sdk/extensions/euphoria/core/translate/BeamFlow.java
+++ sdks/java/extensions/euphoria/euphoria-core/src/main/java/org/apache/beam/sdk/extensions/euphoria/core/translate/BeamFlow.java
@@ -143,8 +143,8 @@ public class BeamFlow extends Flow {
   }
 
   /**
-   * Create flow from input {@link PCollection} which is
-   * {@linkplain PCollection#isFinishedSpecifying() finished specifying}.
+   * Create flow from input {@link PCollection} which is {@linkplain
+   * PCollection#isFinishedSpecifying() finished specifying}.
    *
    * @param pCollection the input {@link PCollection} to wrap into new flow
    * @param <T> type of {@link PCollection} element
@@ -178,11 +178,10 @@ public class BeamFlow extends Flow {
   /**
    * Registers the provided {@link Coder} for the given class.
    *
-   * <p>
-   * Consider using
-   * {@link org.apache.beam.sdk.extensions.euphoria.core.translate.coder.RegisterCoders}
-   * when registering more coders at once.
-   * </p>
+   * <p>Consider using {@link
+   * org.apache.beam.sdk.extensions.euphoria.core.translate.coder.RegisterCoders} when registering
+   * more coders at once.
+   *
    * @param clazz class of element to be encoded/decoded
    * @param coder coder to encode/decode instances of given class
    * @param <T> type parameter of the class to be encoded
@@ -194,11 +193,10 @@ public class BeamFlow extends Flow {
   /**
    * Registers the provided {@link Coder} for the given type.
    *
-   * <p>
-   * Consider using
-   * {@link org.apache.beam.sdk.extensions.euphoria.core.translate.coder.RegisterCoders}
-   * when registering more coders at once.
-   * </p>
+   * <p>Consider using {@link
+   * org.apache.beam.sdk.extensions.euphoria.core.translate.coder.RegisterCoders} when registering
+   * more coders at once.
+   *
    * @param typeDescriptor type of element to be encoded/decoded
    * @param coder coder to encode/decode instances of given class
    * @param <T> type parameter of the class to be encoded
@@ -210,11 +208,9 @@ public class BeamFlow extends Flow {
   /**
    * Registers the provided {@link Coder} for the given class.
    *
-   * <p>
-   * Consider using
-   * {@link org.apache.beam.sdk.extensions.euphoria.core.translate.coder.RegisterCoders}
-   * when registering more coders at once.
-   * </p>
+   * <p>Consider using {@link
+   * org.apache.beam.sdk.extensions.euphoria.core.translate.coder.RegisterCoders} when registering
+   * more coders at once.
    *
    * @param coderProvider
    */
